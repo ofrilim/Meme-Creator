eaddc88799ba3c11db38dc54c1b9b98a60f8dc2e
Limor Ofri <limorofri7@gmail.com>
2019-10-24 20:15:44 +0300

    My second commit!

--- js/main.js
+++ js/main.js
@@ -1,43 +1,90 @@
 'use strict'
 
+var gX, gY;
+var currImg;
+
 var gCanvas = document.getElementById('canvas');
-
 var gCtx = gCanvas.getContext('2d');
-console.log(gCtx)
 
-var elImg = document.getElementById('img-example').src;
-console.log(elImg);
+function init() {
+    createImgs();
+    renderGallery();
+}
+
+// render the img gallery on index.html
+function renderGallery() {
+    var imgs =  getImgs();   
+    var strHTML = imgs.map((img) => {
+
+        return `<img class="img-item" data-id="${img.id}" onclick="onTogglePages(); onImgClicked(this, ${img.id})" src="${img.url}"/>`
+    })
+    document.querySelector('.imgs-container').innerHTML = strHTML.join('');
+}
 
 
-elImg.addEventListener('click', drawImg(this));
+var elInputField = document.getElementById('user-text');
 
-function drawImg(image) {
-    gCtx.drawImage(image, 0, 0, gCanvas.width, gCanvas.height)
+elInputField.addEventListener('input', function() {
+    var userInput = document.getElementById('user-text').value;
+    gMeme.txts[0].line = userInput;
+
+    renderMeme();
+})
+
+// img clicked
+function onImgClicked(elImg, imgId) {       
+    gMeme.selectedImgId = imgId;
+    currImg = elImg;
+    
+    renderMeme();
+}
+
+// render meme and txt to canvas
+function renderMeme() {    
+    drawImgOnCanvas(currImg);
+    txtOnCanvas(gMeme.txts[0].line, gMeme.txts[0].color);
+}
+
+function drawImgOnCanvas(img) {
+    img.width = gCanvas.width;
+    img.height = gCanvas.height;
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+}
+
+// in future that func will get other values as user desire
+function txtOnCanvas(txt, color) {
+    gCtx.font="60px Comic Sans MS";
+    gCtx.fillStyle = color;
+    gCtx.textAlign = "center";
+    gCtx.fillText(txt, 300, 100);
+}
+
+// get the x,y from clicking the canvas - for the next step - controling a few lines of input
+// at the moment - just print the clicked spot
+gCanvas.addEventListener('click', (ev) => {
+    gX = ev.offsetX;
+    gY = ev.offsetY;
+    console.log(gX , gY)
+})
+
+
+function onTogglePages() {
+    var elfirstPage = document.querySelector('.first-page')
+    if (elfirstPage.style.display === "none") elfirstPage.style.display = "block";
+        else elfirstPage.style.display = "none";
+    
+    var elSecondPage = document.querySelector('.second-page');
+    if (elSecondPage.style.display === "none") elSecondPage.style.display = "block";
+        else elSecondPage.style.display = "none";
+}
+
+function onGetColor() {
+    let elUserColor = document.getElementById("user-color").value;
+    return `${elUserColor}`;
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-// function init() {
-//     resize();
+// function onChangeFont() {
 // }
-
-// function resize() {
-//     let elContainer = document.querySelector('.canvas-container');
-    
-//     gCanvas.width = elContainer.offsetWidth;
-//     gCanvas.height = elContainer.offsetHeight;
-// }
-
-
-
-
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,15 +1,42 @@
 // 'use strict';
 
 var gIdx = 1;
-var gImgs; 
+var gImgs;
 
-
-
-function createImgs() {
-    createImg(gIdx, 'meme-imgs/003.jpg', ['happy', 'women']),
-    // createImg(gIdx, 'meme-imgs/3.jpg', ['happy', 'women'])
+var gMeme = {
+    selectedImgId: 1,
+    selectedTxtIdx: 0,
+    txts: [
+        {
+            line: '',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ] 
 }
 
+// get the gImgs objects array
+function getImgs() {
+    return gImgs;
+}
+
+// create an array of objects containing: imgId, imgUrl, words
+function createImgs() {
+    gImgs = [
+        createImg(gIdx++, 'meme-imgs/2.jpg', ['happy', 'woman']),
+        createImg(gIdx++, 'meme-imgs/004.jpg', ['animal', 'dog']),
+        createImg(gIdx++, 'meme-imgs/005.jpg', ['dog', 'baby']),
+        createImg(gIdx++, 'meme-imgs/5.jpg', ['cool', 'winner']),
+        createImg(gIdx++, 'meme-imgs/006.jpg', ['cat', 'animal']),
+        createImg(gIdx++, 'meme-imgs/8.jpg', ['man', 'happy']),
+        createImg(gIdx++, 'meme-imgs/9.jpg', ['funny', 'boy']),
+        createImg(gIdx++, 'meme-imgs/12.jpg', ['man', 'you']),
+        createImg(gIdx++, 'meme-imgs/19.jpg', ['angry', 'man'])
+    ]
+}
+
+// create single img object
 function createImg(id, url, keywords) {
     return {
         id,
@@ -18,5 +45,9 @@ function createImg(id, url, keywords) {
     }
 }
 
+// get the img object by the id
+function getImgById(id) {
+    return gImgs.find((img) => img.id === id) 
+}
 
  
